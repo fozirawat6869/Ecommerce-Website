2ab3d0a113826578f927ee52b55c63fe5742b4de
Demo <fozirawat6869@gmail.com>
2025-10-03 17:14:12 +0530

    handle CasteError :- like invalid id(short id)

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,15 +1,32 @@
-// import handleError from '../utils/handleErrors.js';
+import HandleError from "../utils/handleErrors.js";
+
 
  function error(err,req,res,next){
-        const statusCode=err.statusCode || 500;
-        const message=err.message || "Internal Server Error";
+        let statusCode=err.statusCode || 500;
+        let message=err.message || "Internal Server Error";
+          
+
+        // castError ( invalid mongo id)
+        if(err.name==="CastError"){
+           
+            err=new HandleError(`id is invalid (CastError) : ${err.path}`,404)
+
+             message=err.message
+             statusCode=err.statusCode
+            
+        }
+
         res.status(statusCode).json({
             success:false,
             message:message
+
+            // for those error like if i put short id (casteError)
+            // message:err.stack
         });
     }
 
 export default error;
+
 // We are creating a class named CustomError
 // It gets all the features of the built-in Error class
 // Compare this snippet from backend/utils/handleErrors.js:
