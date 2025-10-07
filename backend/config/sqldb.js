edd77185cb0bb783c34f62558794c610e70c4f42
Demo <fozirawat6869@gmail.com>
2025-10-07 18:40:18 +0530

    create responsive footer

--- backend/config/sqldb.js
+++ backend/config/sqldb.js
@@ -5,14 +5,14 @@ dotenv.config({path:'./config/config.env'})
 
 const connection=mysql2.createConnection({
     host:process.env.host,
-    port:process.env.port,
+    port:process.env.db_port,
     database:process.env.database,
     password:process.env.password,
     user:process.env.user
 })
 
 connection.connect(()=>{
-    console.log(`database connected to  ${process.env.port}`)
+    console.log(`database connected to  ${process.env.db_port}`)
 })
 
 export default connection;
