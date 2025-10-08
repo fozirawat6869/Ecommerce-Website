c711241248f0ae92395cce39ad960cab9df7ea3d
Demo <fozirawat6869@gmail.com>
2025-10-08 13:39:12 +0530

    ImageSlider is done

--- e-commerce/src/components/home/Home.jsx
+++ e-commerce/src/components/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Footer from '../footer/Footer'
 import Nav from '../nav/nav'
+import ImageSlider from '../imageSlider/ImageSlider'
 
 function Home() {
   return (
     <>
     <Nav/>
+    <ImageSlider/>
     <div className='bg-yellow-200 p-5'>
       <h1 className='text-3xl bg-red-300 text-center p-5'>Home component</h1>
     </div>
---
+++ e-commerce/src/components/imageSlider/ImageSlider.jsx
@@ -0,0 +1,50 @@
+
+import React, { useState, useEffect } from 'react'
+import img1 from '../images/img1.jpg'
+import img2 from '../images/img2.jpg'
+import img3 from '../images/img3.png'
+import img4 from '../images/img4.png'
+
+const images = [img1, img2, img3, img4]
+
+export default function ImageSlider() {
+    
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  useEffect(() => {
+   
+    return () => clearInterval(interval)
+  }, [])
+
+  return (
+    <div className="relative w-full overflow-hidden">
+      {/* Slider */}
+      <div
+        className="flex transition-transform duration-700 ease-in-out"
+        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+      >
+        {images.map((img, i) => (
+          <img
+            key={i}
+            src={img}
+            alt={`Slide ${i + 1}`}
+            className="w-full lg:h-[500px] md:h-[350px] sm:h-[350px] h-[200px] object-cover flex-shrink-0"
+          />
+        ))}
+      </div>
+
+      {/* Dots */}
+      <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 flex gap-3">
+        {images.map((_, i) => (
+          <span
+            key={i}
+            onClick={() => setCurrentIndex(i)}
+            className={`w-3 h-3 rounded-full cursor-pointer ${
+              currentIndex === i ? 'bg-white scale-125' : 'bg-gray-400'
+            }`}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
