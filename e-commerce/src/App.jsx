226059d4ec2246064196716a86fce7df316d2d22
Demo <fozirawat6869@gmail.com>
2025-11-08 23:42:31 +0530

    added lazy loading in AllProduct component for optimizations

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,15 +1,24 @@
-
+import { lazy, Suspense } from 'react'
 import './App.css'
-import Home from './components/home/Home'
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import AllProducts from './components/allProducts/allProducts'
-import NewlyProducts from './components/newelyCreatedProducts/newelyProducts'
+
+
 import Footer from './components/footer/Footer'
 import Nav from './components/nav/Nav'
-import ProductDetails from './components/pages/ProductDetails'
+
 import { QueryClient,QueryClientProvider } from '@tanstack/react-query'
+
+const Home=lazy(()=>import('./components/home/Home'))
+const AllProduct=lazy(()=>new Promise((resolve)=>{
+   setTimeout(()=>{resolve(import('./components/allProducts/AllProduct'))},5000)
+}))
+// const AllProducts=lazy(()=>import('./components/allProducts/AllProducts'))
+const ProductDetails=lazy(()=>import('./components/pages/ProductDetails'))
+// const NewelyProducts=lazy(()=>import('./components/newelyCreatedProducts/NewlyProducts'))
+
 function App() {
-   
+    
   const queryClient=new QueryClient()
 
   return (
@@ -17,14 +26,17 @@ function App() {
     <QueryClientProvider client={queryClient}>
   <BrowserRouter>
   <Nav/>
+  {/* <Suspense fallback={<div>Loading...</div>}> */}
+          <Suspense fallback={<div style={{ textAlign: 'center', marginTop: '50px',color:'black' }} className='text-center text-black text-6xl'>⏳ Loading...</div>}>
     <Routes>
       <Route path='/' element={<Home/>}/>
      
-      <Route path='/AllProducts' element={<AllProducts/>}/>
+      <Route path='/AllProducts' element={<AllProduct/>}/>
       <Route path='/product/:id' element={<ProductDetails/>}/>
-      <Route path='/newelyProducts' element={<NewlyProducts/>}/>
+      {/* <Route path='/newelyProducts' element={<NewelyProducts/>}/> */}
       
     </Routes>
+    </Suspense>
      <Footer/>
   </BrowserRouter>
   </QueryClientProvider>
