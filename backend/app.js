abd78c682f3101c2e74cc19ee11d92dc2d813475
Demo <fozirawat6869@gmail.com>
2026-04-07 05:42:48 +0530

    update API URL from localhost to Railway and fix CORS for Vercel

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ import cors from 'cors'
 const app=express();
  
 app.use(cors({
-    origin:"http://localhost:5173"
+    origin:"ecommerce-website-khpyq541c-mayur-rawats-projects.vercel.app"
 }))
 
 app.use(express.json())
--- e-commerce/src/utils/api.js
+++ e-commerce/src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:8000";
+// export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: BASE_URL
