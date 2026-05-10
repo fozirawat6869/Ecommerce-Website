89eac8c434172cd69a8006c3b6256d59b46262da
Demo <fozirawat6869@gmail.com>
2026-05-10 20:43:40 +0530

    added usenavigate after logout

--- e-commerce/src/components/pages/UserProfile.jsx
+++ e-commerce/src/components/pages/UserProfile.jsx
@@ -2,14 +2,15 @@
 
 import { CgProfile } from "react-icons/cg";
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
 import api from "../../utils/api"; // ✅ added
 import UserLoader from '../../Loaders/ForUserProfile'
-import { Link } from "react-router-dom";
+
 
 function UserProfile() {
+  const navigate = useNavigate();
 
   const [editButton,setEditButton]=useState({
      forName:false,
@@ -303,7 +304,7 @@ return (
 
             {/* MOBILE */}
             <div>
-              <div className="flex items-center gap-5 mb-2">
+              <div className="flex items-center gap-5 mb-3">
                 <h2 className="font-semibold text-gray-700">Mobile Number</h2>
                   </div>
               <input
@@ -315,9 +316,16 @@ return (
             </div>
 
             <div className=" mb-2">
-                <Link to="/logout" className="bg-red-500 text-white font-bold px-3 py-2 rounded-2xl ">
+                <button 
+                onclick={()=>{
+                  localStorage.removeItem("token")
+                    navigate("/");
+                    window.location.reload();
+                }   
+                }
+                className="bg-red-500 text-white font-bold px-3 py-2 rounded-xl cursor-pointer hover:scale-105 transition-transform"> 
                   Logout
-                </Link>
+                </button>
             </div>
 
 
