9d53a5bdb674392a0585adf6f296a8f53b29ff68
Demo <fozirawat6869@gmail.com>
2026-06-21 23:00:23 +0530

    create a seperate FroOrderDetails.jsx in Loaders for OrderDetails component

--- e-commerce/src/components/pages/OrderDetails.jsx
+++ e-commerce/src/components/pages/OrderDetails.jsx
@@ -9,6 +9,7 @@ import {
   FaCheckCircle,
 } from "react-icons/fa";
 import api,{BASE_URL} from "../../utils/api";
+import ForOrderDetails from "../../Loaders/ForOrderDetails";
 
 
 function OrderDetails() {
@@ -36,10 +37,7 @@ console.log(res.data.orderDetails)
 
   if(isLoading){
   return (
-    <div className="flex flex-col items-center justify-center h-[70vh] gap-3">  
-      <div className="w-10 h-10 border-4 border-gray-200 border-t-blue-600 rounded-full animate-spin" />
-      <p className="text-gray-400 text-sm">Loading your order details...</p>
-    </div>
+   <ForOrderDetails/>
   )
 }
 
